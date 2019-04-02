814894be4132822f3e2192399faca76e14d31037
drewdearing <social@drewdearing.com>
2019-04-02 09:58:58 -0500

    make keyboard go away

--- pitchnotes/Controllers/ProfileSetupViewController.swift
+++ pitchnotes/Controllers/ProfileSetupViewController.swift
@@ -212,14 +212,14 @@ class ProfileSetupViewController: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // code to dismiss keyboard when user clicks on background
+    func textFieldShouldReturn(textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
