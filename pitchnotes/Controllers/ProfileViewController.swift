0d68c39fc4305ce1a20900c64706433b9193680f
Xi Jin <jinxi@Xis-MacBook-Pro.local>
2019-04-15 23:03:25 -0500

    profile page update

--- pitchnotes/Controllers/ProfileViewController.swift
+++ pitchnotes/Controllers/ProfileViewController.swift
@@ -11,42 +11,57 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var navBarItem: UINavigationItem!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var skillsLabel: UILabel!
-    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var gradyearLabel: UILabel!
+    @IBOutlet weak var bioTextView: UITextView!
+    @IBOutlet weak var skill1Label: UILabel!
+    @IBOutlet weak var skill2Label: UILabel!
+    @IBOutlet weak var skill3Label: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if let profile = getCurrentProfile() {
-            nameLabel.text = profile.name + ", Class of " + String(profile.graduate)
-            emailLabel.text = profile.schoolEmail
-            bioLabel.text = profile.bio
-            skillsLabel.text = profile.skills.joined(separator: ", ")
-            
             let url = URL(string: profile.photoURL)
             let data = try? Data(contentsOf: url!)
             if let imgData = data {
                 let image = UIImage(data:imgData)
-                imageView.image = image
+                profileImage.image = image
             }
+            profileImage.layer.borderWidth = 1
+            profileImage.layer.masksToBounds = false
+            profileImage.layer.borderColor = UIColor.black.cgColor
+            profileImage.layer.cornerRadius = profileImage.frame.height/2
+            profileImage.clipsToBounds = true
+            
+            nameLabel.text = profile.name
+            gradyearLabel.text = "Class of \(profile.graduate)"
+            bioTextView.text = profile.bio
+            skill1Label.text = profile.skills[0]
+            skill2Label.text = profile.skills[1]
+            skill3Label.text = profile.skills[2]
+            
         }
+        
+//        if let profile = getCurrentProfile() {
+//            nameLabel.text = profile.name + ", Class of " + String(profile.graduate)
+//            emailLabel.text = profile.schoolEmail
+//            bioLabel.text = profile.bio
+//            skillsLabel.text = profile.skills.joined(separator: ", ")
+//
+//            let url = URL(string: profile.photoURL)
+//            let data = try? Data(contentsOf: url!)
+//            if let imgData = data {
+//                let image = UIImage(data:imgData)
+//                imageView.image = image
+//            }
+//        }
     }
     
     @IBAction func sideButton(_ sender: Any) {
         sideMenuController?.revealMenu()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
